3b5461a78f4e6fa379b82d5ab8c5823ca44f992c
pasor1 <pasor1@gmail.com>
2021-01-20 20:52:09 +0100

    improve navigation

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -16,8 +16,8 @@ function App() {
       <header className="text-center pt-2 mb-7 bg-gradient-to-b from-purple-200 to-gray-200">
         <Link to="/"><img className="m-auto" style={{ width: `700px` }} alt="Avatar Generator" src={titleLogo} /></Link>
         <nav className="my-7">
-          <NavLink to="/" exact activeClassName="border-purple-400 font-semibold" className="mx-3 p-4 border-b-2 bg-purple-700 bg-opacity-10 rounded-t hover:border-purple-700 text-purple-900">Avatars</NavLink>
-          <NavLink to="/new-avatar" activeClassName="border-purple-400 font-semibold" className="mx-3 p-4 border-b-2 bg-purple-700 bg-opacity-10 rounded-t hover:border-purple-700 text-purple-900">Generator</NavLink>
+          <NavLink to="/" exact activeClassName="border-purple-900 font-semibold" className="mx-3 p-4 border-b-2 bg-purple-700 bg-opacity-10 rounded-t hover:border-purple-700 text-purple-900">Avatars</NavLink>
+          <NavLink to="/new-avatar" activeClassName="border-purple-900 font-semibold" className="mx-3 p-4 border-b-2 bg-purple-700 bg-opacity-10 rounded-t hover:border-purple-700 text-purple-900">Generator</NavLink>
         </nav>
       </header>
 
